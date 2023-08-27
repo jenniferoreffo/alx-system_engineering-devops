67de41813043ccf71585133cb32dc1f8bba42227
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-08-27 21:02:45 +0000

    Commit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,10 @@
-# Puppet script to creat ssh config file
+# Puppet script to create ssh config file
 file_line { 'Turn off password auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
+
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
