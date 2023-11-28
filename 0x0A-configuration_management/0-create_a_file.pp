50b7cfd83862db0517dd8979000edce109d799fb
Jennifer <jennyoreffo@gmail.com>
2023-11-27 16:37:12 -0800

    commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # This manifest creates a file at /tmp
-file { '/tmp/school': 
-    ensure  => 'file',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+file { '/tmp/school':
+  ensure   => 'file',
+  mode     => '0744',
+  owner    => 'www-data',
+  group    => 'www-data',
+  content  => 'I love Puppet',
 }
-
