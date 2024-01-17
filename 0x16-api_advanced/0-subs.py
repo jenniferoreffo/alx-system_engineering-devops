f16e71269986ff16c07cc17be6794d3071f2114f
Jennifer Oreffo <jennyoreffo@gmail.com>
2024-01-17 02:19:38 -0800

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,20 +2,24 @@
 """  a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit."""
 
 from requests import get
-from json import loads
 
 def number_of_subscribers(subreddit):
     """queries to Reddit Api"""
-    u_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    headers = {
-            'User-Agent': u_agent
-    }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = get(url, headers=headers)
-    reddits = response.json()
 
-    try:
-        subscribers = reddits.get('data').get('subscribers')
-        return int(subscribers)
-    except:
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    req = get(url, headers=headers, allow_redirects=False)
+
+    if req.status_code != 200:
         return 0
+    try:
+        jsonf = req.json()
+    except ValueError:
+        return 0    
+    data = jsonf.get("data")
+    if data:
+        sub_count = data["subscribers"]
+        if sub_count:
+            return sub_count
+    return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
 """ a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit. 
 """
-import sys
-import requests
+
+from requests import gets
 
 def top_ten(subreddit)
     """queries to Reddit Api"""
-    user_agent = 'Mozilla/5.0'
-    headers = {
-        'User-Agent': user_agent
-    }
-    params = {
-        'limit': 10 
-    }
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url, 
-            headers=headers
-            params=params,
-            allow_redirects=False)
-    if res.status_code != 200:
-        return 0
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    if len(hot_posts) is 0:
-        print(None)
-    else:
-        for post in hot_posts:
-            print(post['data']['title'])
-
+      url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+      headers = {'user-agent': 'my-app/0.0.1'}
+      req = get(url, headers=headers,
+              allow_redirects=False, params={"limit": 10})
+      if req.status_code != 200:
+          print(None)
+          return None
+      try:
+          jsonf = req.json()
+      except ValueError:
+          print(None)
+          return None
+      try:
+          data = jsonf.get("data")
+          children = data.get("children")
+          for child in children:
+              post = child.get("data")
+              print(post.get("title"))
+      except Exception:
+          print(None)
