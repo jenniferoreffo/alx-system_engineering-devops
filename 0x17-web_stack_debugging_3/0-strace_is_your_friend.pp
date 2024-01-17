75f999805f9a76abf08c7afb8a48f3e86b7710a5
Jennifer Oreffo <jennyoreffo@gmail.com>
2024-01-17 02:20:38 -0800

    commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,12 @@
 #Using strace, find out why Apache is returning a 500 error.
 #fix apache errors
 
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+exec { 'fix-wordpress':
+  environment => ['DIR=/var/www/html/wp-settings.php',
+'OLD=phpp',
+'NEW=php'],
+
+command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
+path        => ['/usr/bin', '/bin'],
+returns     => [0, 1]
 }
